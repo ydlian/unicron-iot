91c117cbaa327eb173e5691901e8207b3ebc8a47
ydlian <lianyadong09@163.com>
2024-11-01 21:24:54 +0800

    Update OrangeConst.java

--- unicron-mqtt-common/src/main/java/com/me/unicron/iot/constant/OrangeConst.java
+++ unicron-mqtt-common/src/main/java/com/me/unicron/iot/constant/OrangeConst.java
@@ -2,5 +2,5 @@ package com.me.unicron.iot.constant;
 
 public class OrangeConst {
 
-	public static final String ORANGE_COMPANY_ID = "91110113MA01CF8F83";
+	public static final String ORANGE_COMPANY_ID = "XX110113MA01CF8F83";
 }
